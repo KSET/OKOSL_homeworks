39ff25859efc035c69ae73cac1b49c76a6db8ed2
LVH-27 <leonard.volaric-horvat@fer.hr>
2019-08-07 11:49:06 +0200

    Added user config

--- config.py
+++ config.py
@@ -2,8 +2,18 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "my-pass-27"
+    """
+    This class contains configuration key-value pairs, for various features
+    used throughout the app.
+
+    Most notably, the secret key, the DB settings and some user-related settings are configured.
+    """
+
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "my-pass-pimpek-penis-27"
     # URI format: dialect+driver://username:password@host:port/database
+    SQLALCHEMY_TRACK_MODIFICATIONS = False    # Avoids SQLAlchemy warning
+
+    # PostgreSQL settings
     DB_USER = os.environ.get("DB_USER") or "okosl"
     DB_PASSWORD = os.environ.get("DB_PASSWORD")
     DB_HOST = os.environ.get("DB_HOST")
@@ -14,3 +24,10 @@ class Config(object):
                                                                    DB_HOST,
                                                                    DB_PORT,
                                                                    DB_NAME)
+
+    # Flask-User settings - see https://flask-user.readthedocs.io/en/v0.6/customization.html
+    # Should mail-related features be used, a SMTP server needs to be specified using e.g. Flask-Mail
+    USER_APP_NAME = "OKOSL Homeworks"  # Shown in and email templates and page footers
+    USER_ENABLE_USERNAME = True
+    USER_ENABLE_EMAIL = False
+    USER_ENABLE_REGISTRATION = False  # disable user registration through the website - an admin will add users by hand
