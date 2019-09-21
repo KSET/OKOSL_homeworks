3332d0863b43127cdad126523641a68a496442a0
jakov <jakov@dream-implementation.com>
2019-09-21 15:49:02 +0200

    Add cloning repos

---
+++ git-engine/repos.py
@@ -0,0 +1,31 @@
+import requests
+import json
+import os
+import subprocess
+
+class Repos():
+    def __init__(self, homework_name):
+        self.homework_name = homework_name
+        self.api_url = 'https://'+os.environ['GITEA_HOST']+'/api/v1'
+        self.headers = {'Authorization': 'token '+os.environ['GITEA_TOKEN']}
+        self.repo_location = os.environ['REPO_LOCATION']
+        self.repos = []
+
+    def clone_n_parse(self):
+        pass
+
+    def search(self):
+        response = requests.get(
+                self.api_url+'/repos/search',
+                headers = self.headers,
+                params = {'q': self.homework_name})
+        
+        json_response = json.loads(response.text)
+        self.repos = json_response['data']
+
+    def clone(self):
+        for repo in self.repos:
+            subprocess.run(['git', 'clone',
+                repo['ssh_url'],
+                self.repo_location+'/'+repo['owner']['login']])
+
