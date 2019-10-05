15767aa652b836cb2db949ff71da5130b0e290d6
LVH-27 <leonard.volaric-horvat@fer.hr>
2019-10-05 17:53:50 +0200

    Revised models to better link students with their solutions

--- app/models.py
+++ app/models.py
@@ -23,7 +23,9 @@ class Homework(db.Model):
     # and but the sole benefit would be getting all years in the database, and there will be only a
     # few homeworks per year, so it is probably not worth the increase in complexity
     year = db.Column(db.Integer, nullable=False)
+
     tasks = db.relationship("Task", backref="homework", lazy="dynamic")
+    solved_homeworks = db.relationship("SolvedHomework", backref="homework")
 
     def __repr__(self):
         """
@@ -41,15 +43,29 @@ class Homework(db.Model):
 
 
 class Task(db.Model):
-    """Task model. Each task has its number, text, and the ID of the homework it belongs to."""
+    """Task model. Each homework is divided into tasks, which roughly correspond to a single topic
+    (e.g. tasks with sed or user creation). Each task is divided into subtasks."""
 
     __tablename__ = "tasks"
     id = db.Column(db.Integer, primary_key=True)
-    task_number = db.Column(db.Integer, nullable=False)
+    task_number = db.Column(db.Integer, nullable=False)  # the task's homework-level ordinal number
+
+    # task-level note, e.g. "All subtasks pertain to the file /usr/share/dict/words"
     task_text = db.Column(db.Text, nullable=False)
-    max_points = db.Column(db.Integer, nullable=False)
+
+    # filename that should exist in the repo and contain the task solution
+    solution_filename = db.Column(db.Text, nullable=False)
+
     homework_id = db.Column(db.Integer, db.ForeignKey("homeworks.id"), nullable=False)
-    solution_groups = db.relationship("SolutionGroup", backref="task", lazy="dynamic")
+    subtasks = db.relationship("Subtask", backref="task", lazy="dynamic")
+
+    def __init__(self, *args, **kwargs):
+        """
+        Overriding to add default solution filename if empty - set it to "task_{number}.sh"
+        """
+        if 'solution_filename' not in kwargs:
+            kwargs['solution_filename'] = f'task_{kwargs["task_number"]}.sh'
+        super().__init__(*args, **kwargs)
 
     def __repr__(self):
         """
@@ -58,16 +74,38 @@ class Task(db.Model):
         return f'<HWID:{self.homework_id};Task#{self.task_number}>'
 
 
+class Subtask(db.Model):
+    """Subtask model. Each subtask has its number, text, and the ID of the Task it belongs to."""
+
+    __tablename__ = "subtasks"
+    id = db.Column(db.Integer, primary_key=True)
+    subtask_number = db.Column(db.Integer, nullable=False)
+    subtask_text = db.Column(db.Text, nullable=False)
+    max_points = db.Column(db.Integer, nullable=False)
+    task_id = db.Column(db.Integer, db.ForeignKey("tasks.id"), nullable=False)
+
+    solution_groups = db.relationship("SolutionGroup", backref="subtask", lazy="dynamic")
+
+    def __repr__(self):
+        """
+        Override the default string representation method
+        """
+        return f'<TaskID:{self.task_id};Task#{self.subtask_number}>'
+
+
 class SolutionGroup(db.Model):
-    """Solution Group model. Each task has several possible solutions, correct or otherwise.
+    """Solution Group model. Each subtask has several possible solutions, correct or otherwise.
     However, some solutions may be syntactically different, but functionally identical
     (e.g. ls -al and ls -la), so it makes sense to group these. This model enables it.
     """
 
     __tablename__ = "solution_groups"
     id = db.Column(db.Integer, primary_key=True)
-    task_id = db.Column(db.Integer, db.ForeignKey("tasks.id"), nullable=False)
-    solutions = db.relationship("Solution", backref="task", lazy="dynamic")
+    subtask_id = db.Column(db.Integer, db.ForeignKey("subtasks.id"), nullable=False)
+
+    solutions = db.relationship("Solution", backref="solution_group", lazy="dynamic")
+    remarks = db.relationship("Remark", backref="solution_group", lazy="dynamic")
+
     # the following fields should be filled once a final, aggregate remark is written
     final_remark = db.Column(db.Text, nullable=True)
     final_score_percentage = db.Column(db.Float(), nullable=True)
@@ -81,7 +119,7 @@ class SolutionGroup(db.Model):
         """
         Override the default string representation method
         """
-        return f'<SGID:{self.id};TaskID:{self.task_id}>'
+        return f'<SGID:{self.id};SubtaskID:{self.subtask_id}>'
 
 
 class Remark(db.Model):
@@ -97,6 +135,7 @@ class Remark(db.Model):
     score_percentage = db.Column(db.Float(), nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
     solution_group_id = db.Column(db.Integer, db.ForeignKey("solution_groups.id"), nullable=False)
 
     __table_args__ = (
@@ -114,8 +153,8 @@ class Remark(db.Model):
 class Solution(db.Model):
     """
     Solution model. Each solution should be uniquely determined by its text and the solution group it belongs to.
-    Several tasks (e.g. recycled tasks from previous years) may have the same solution, so imposing a unique constraint
-    on solution_text would muddy the model up.
+    Several subtasks (e.g. recycled subtasks from previous years) may have the same solution, so imposing a unique
+    constraint on solution_text would muddy the model up.
     A solution may be functionally identical to other solutions, so each solution belongs to a single SolutionGroup.
     """
 
@@ -126,6 +165,8 @@ class Solution(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     solution_text = db.Column(db.Text, nullable=False, unique=False)
     solution_group_id = db.Column(db.Integer, db.ForeignKey("solution_groups.id"), nullable=False)
+
+    solved_homeworks = db.relationship("SolvedHomeworkSolution", back_populates="solution")
 
     def __repr__(self):
         """
@@ -146,23 +187,39 @@ class Student(db.Model):
     first_name = db.Column(db.String(100), nullable=True)
     last_name = db.Column(db.String(100), nullable=True)
 
+    solved_homeworks = db.relationship("SolvedHomework", backref="student")
 
-class StudentSolution(db.Model):
+
+class SolvedHomework(db.Model):
     """
-    Association model which links students to their solutions. There is an additional homework_id
-    field to make pushing remarks for a given homework faster. The alternative is querying up the chain
-    from Solution to SolutionGroup to Task, just to check which homework is being marked.
+    Model which models a solved homework as a whole. It links students with homeworks, and points to the solutions to
+    all the tasks for a given homework. There is an additional repo_url field which contains the URL to the repo.
     """
 
-    __tablename__ = "students_solutions"
+    __tablename__ = "solved_homeworks"
 
     id = db.Column(db.Integer, primary_key=True)
     student_id = db.Column(db.Integer, db.ForeignKey("students.id"), nullable=False)
-    solution_id = db.Column(db.Integer, db.ForeignKey("solutions.id"), nullable=False)
     homework_id = db.Column(db.Integer, db.ForeignKey("homeworks.id"), nullable=False)
 
-    students = db.relationship(Student, backref="students_solutions")
-    solutions = db.relationship(Solution, backref="students_solutions")
+    repo_url = db.Column(db.String(255), nullable=False)
+    # points = db.Column(db.Float(), nullable=True)
+    solutions = db.relationship("SolvedHomeworkSolution", back_populates="solved_homework")
+
+
+class SolvedHomeworkSolution(db.Model):
+    """
+    Association model which links solved homeworks to the solutions contained in them.
+    """
+
+    __tablename__ = "solved_homeworks_solutions"
+
+    id = db.Column(db.Integer, primary_key=True)
+    solved_homework_id = db.Column(db.Integer, db.ForeignKey("solved_homeworks.id"), nullable=False)
+    solution_id = db.Column(db.Integer, db.ForeignKey("solutions.id"), nullable=False)
+
+    solved_homework = db.relationship("SolvedHomework", back_populates="solutions")
+    solution = db.relationship("Solution", back_populates="solved_homeworks")
 
 
 class User(db.Model, UserMixin):
