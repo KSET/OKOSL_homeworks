42501a11f4c3f4959ad5da1869152d6985e9fee1
LVH-27 <leonard.volaric-horvat@fer.hr>
2019-10-21 23:39:27 +0200

    Added final remark functionality

--- app/forms.py
+++ app/forms.py
@@ -11,4 +11,15 @@ class RemarkForm(FlaskForm):
 
     remark_text = TextAreaField(label="Remark text", validators=[DataRequired()])
     remark_score_percentage = DecimalField(label="Score percentage", validators=[DataRequired(), NumberRange(min=0, max=1.5)])
-    submit = SubmitField("Submit remark")
+    submit_remark = SubmitField("Submit remark")
+
+
+class FinalRemarkForm(FlaskForm):
+    """
+    This class defines a form for inputting final remarks. It is structurally identical to RemarkForm, but reusing it
+    caused problems with passing data. It should be looked into in the future.
+    """
+
+    remark_text = TextAreaField(label="Final remark text", validators=[DataRequired()])
+    remark_score_percentage = DecimalField(label="Score percentage", validators=[DataRequired(), NumberRange(min=0, max=1.5)])
+    submit_final_remark = SubmitField("Submit final remark")
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from app import app, db
 from flask import render_template  # , flash
 from flask_user import login_required, roles_required, current_user
 from .models import Homework, Task, Subtask, SolutionGroup, Remark  # , Solution, Remark
-from .forms import RemarkForm
+from .forms import RemarkForm, FinalRemarkForm
 # import datetime
 
 
@@ -48,34 +48,90 @@ def solution_group(task_id, subtask_id, sg_id):
     solution_group = SolutionGroup.query.get(sg_id)
     subtask = Subtask.query.get(subtask_id)
     task = Task.query.get(task_id)
-    form = RemarkForm()
+    remark_form = RemarkForm()
+    final_remark_form = FinalRemarkForm()
+    author = current_user
+    remark_added = False
+    final_remark_added = False
 
-    if form.validate_on_submit():
-        author = current_user
-        remark_text = form.remark_text.data
-        remark_score_percentage = form.remark_score_percentage.data
+    if remark_form.validate_on_submit() and remark_form.submit_remark.data:
+        target_form = remark_form
+        remark_added = True
+    elif final_remark_form.validate_on_submit() and final_remark_form.submit_final_remark.data:
+        target_form = final_remark_form
+        final_remark_added = True
+
+    if remark_added or final_remark_added:
+        remark_text = target_form.remark_text.data
+        remark_score_percentage = target_form.remark_score_percentage.data
         remark = Remark(author=author,
                         text=remark_text,
                         score_percentage=remark_score_percentage,
                         solution_group=solution_group
                         )
 
-        db.session.add(remark)
-        db.session.commit()
-        return render_template('solution_group_page.html',
-                               form=form,
-                               solution_group=solution_group,
-                               subtask=subtask,
-                               task=task,
-                               homework=homework
-                               )
+        if remark_added:
+            db.session.add(remark)
+            db.session.commit()
+        else:
+            if solution_group.final_remark is not None:
+                db.session.delete(solution_group.final_remark)
+            solution_group.final_remark = remark
+            db.session.add(solution_group)
+            db.session.commit()
     return render_template('solution_group_page.html',
-                           form=form,
+                           remark_form=remark_form,
+                           final_remark_form=final_remark_form,
                            solution_group=solution_group,
                            subtask=subtask,
                            task=task,
                            homework=homework
                            )
+
+
+# @app.route('/tasks/<task_id>/subtask_<subtask_id>/solution_group_<sg_id>', methods=['POST'])
+# @login_required
+# def add_remark(task_id, subtask_id, sg_id):
+#     flash("Adding remark")
+#     print("Adding remark")
+#     form = RemarkForm()
+#     author = current_user
+
+#     if form.validate_on_submit():
+#         remark_text = form.remark_text.data
+#         remark_score_percentage = form.remark_score_percentage.data
+#         remark = Remark(author=author,
+#                         text=remark_text,
+#                         score_percentage=remark_score_percentage,
+#                         solution_group=solution_group
+#                         )
+#         db.session.add(remark)
+#         db.session.commit()
+#         flash("Remark added!", 'success')
+#     return redirect(url_for('solution_group', task_id=task_id, subtask_id=subtask_id, sg_id=sg_id))
+
+
+# @app.route('/tasks/<task_id>/subtask_<subtask_id>/solution_group_<sg_id>', methods=['POST'])
+# @login_required
+# def add_final_remark(task_id, subtask_id, sg_id):
+#     flash("Adding final remark")
+#     print("Adding final remark")
+#     form = FinalRemarkForm()
+#     author = current_user
+
+#     if form.validate_on_submit():
+#         remark_text = form.remark_text.data
+#         remark_score_percentage = form.remark_score_percentage.data
+#         remark = Remark(author=author,
+#                         text=remark_text,
+#                         score_percentage=remark_score_percentage,
+#                         solution_group=solution_group
+#                         )
+#         solution_group.final_remark = remark
+#         db.session.add(solution_group)
+#         db.session.commit()
+#         flash("Final remark added!", 'success')
+#     return redirect(url_for('solution_group', task_id=task_id, subtask_id=subtask_id, sg_id=sg_id))
 
 
 @app.route('/admin')
