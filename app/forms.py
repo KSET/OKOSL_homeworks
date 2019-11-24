b963bc6d489fe121f0aadbe639f6f022cdb6b4b7
LVH-27 <leonard.volaric-horvat@fer.hr>
2019-11-24 12:38:14 +0100

    Fixed form not accepting 0; remark text no longer required.
    The validator DataRequired() does `if field.data`, which evaluates 0 to False and thus rejects a (valid) input.
    Therefore, InputRequired() must be used instead.
    Some remarks do not warrant a text (e.g. an expected correct solution), so the text is no longer required.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, DecimalField, SubmitField
-from wtforms.validators import DataRequired, NumberRange
+from wtforms.validators import InputRequired, NumberRange
 
 
 class RemarkForm(FlaskForm):
@@ -9,8 +9,9 @@ class RemarkForm(FlaskForm):
     The author, date and solution group do not have their fields because they can be inferred from the context.
     """
 
-    remark_text = TextAreaField(label="Remark text", validators=[DataRequired()])
-    remark_score_percentage = DecimalField(label="Score percentage", validators=[DataRequired(), NumberRange(min=0, max=1.5)])
+    remark_text = TextAreaField(label="Remark text", validators=[])
+    remark_score_percentage = DecimalField(label="Score percentage",
+                                           validators=[InputRequired(), NumberRange(min=0, max=1.5)])
     submit_remark = SubmitField("Submit remark")
 
 
@@ -20,6 +21,7 @@ class FinalRemarkForm(FlaskForm):
     caused problems with passing data. It should be looked into in the future.
     """
 
-    remark_text = TextAreaField(label="Final remark text", validators=[DataRequired()])
-    remark_score_percentage = DecimalField(label="Score percentage", validators=[DataRequired(), NumberRange(min=0, max=1.5)])
+    remark_text = TextAreaField(label="Final remark text", validators=[InputRequired()])
+    remark_score_percentage = DecimalField(label="Score percentage",
+                                           validators=[InputRequired(), NumberRange(min=0, max=1.5)])
     submit_final_remark = SubmitField("Submit final remark")
